763938e3d6ca4e4670b073699304fcedad3ecc44
Margaret142 <tabbywange75@gmail.com>
2025-08-31 20:46:23 +0300

    Clean up working tree

--- ALX-Capstone-Project/src/components/tracks/TrackList.jsx
+++ ALX-Capstone-Project/src/components/tracks/TrackList.jsx
@@ -1,22 +1,3 @@
-// import React from 'react'
-
-// function TrackList() {
-//   return (
-//     <div>
-//       <h2 className="text-xl font-bold mb-2">Track List</h2>
-//       <ul className="list-disc pl-5">
-//         <li>Track 1</li>
-//         <li>Track 2</li>
-//         <li>Track 3</li>
-//       </ul>
-//     </div>
-//   )
-// }
-
-// export default TrackList
-
-
-// src/components/TrackList.jsx
 const TrackList = ({ tracks, onTrackSelect }) => {
   if (tracks.length === 0) {
     return (
